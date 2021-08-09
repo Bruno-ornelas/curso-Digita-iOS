dbf3d882d0321fe8178ce18a254a3c9cc5aa9b65
bruno <bruno.p.ornelas@gmail.com>
2021-08-09 19:46:16 -0300

    valida botão e esconde teclado

--- cursoIos/cursoIos/ViewController.swift
+++ cursoIos/cursoIos/ViewController.swift
@@ -10,17 +10,48 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    //Outlet componentes da tela
     @IBOutlet weak var ibCpf: UITextField!
     @IBOutlet weak var ibCelular: UITextField!
     @IBOutlet weak var ibCep: UITextField!
-    
     @IBOutlet weak var ibEmail: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+
     }
-
-
+    
+    
+    
+    
+    @IBAction func actionButtonRegister(_ sender: Any) {
+        
+        shouldColorBorderTexfildIsNull(do: ibCpf)
+        
+    }
+    
+    
+    
+    //MARK: Metodos
+    
+    func shouldColorBorderTexfildIsNull(do textfiled : UITextField) {
+        
+        if textfiled.text == "" {
+            textfiled.layer.borderColor = UIColor.red.cgColor
+            textfiled.layer.borderWidth = 1.0
+        }   else {
+            textfiled.layer.borderWidth = 1.0
+            textfiled.layer.borderColor = UIColor.clear.cgColor
+        }
+                
+    }
+    
+    
+    func isvalidCpf() -> Bool {
+        return false
+    }
 }
-
